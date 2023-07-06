37b13eec5700ba1b328258ee499ad6c9f863281f
AstikJun <serikovastik@gmail.com>
2023-07-06 11:18:14 +0600

    Finished testing

--- src/test/java/com/example/springrestpractice/controller/TasksRestControllerIT.java
+++ src/test/java/com/example/springrestpractice/controller/TasksRestControllerIT.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvc.*;
@@ -16,8 +17,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
@@ -37,28 +38,95 @@ class TasksRestControllerIT {
         this.taskRepository.getTasks()
                 .addAll(List.of(new Task(UUID.fromString("c5c8cc73-8724-491f-afba-143aea480aee"), "Первая задача", false),
                         new Task(UUID.fromString("0742f17e-aac5-4681-8596-30f57d6ac680"), "Вторая задача", true)
-        ));
+                ));
 
         //when
         this.mockMvc.perform(requestBuilder)
-        //then
+                //then
                 .andExpectAll(
                         status().isOk(),
                         content().contentType(MediaType.APPLICATION_JSON),
                         content().json("""
-[
-{
-"id": "c5c8cc73-8724-491f-afba-143aea480aee",
-"details": "Первая задача",
-"completed": false
-},
-{
-"id": "0742f17e-aac5-4681-8596-30f57d6ac680",
-"details": "Вторая задача",
-"completed": true
-}
-]
-""")
+                                [
+                                {
+                                "id": "c5c8cc73-8724-491f-afba-143aea480aee",
+                                "details": "Первая задача",
+                                "completed": false
+                                },
+                                {
+                                "id": "0742f17e-aac5-4681-8596-30f57d6ac680",
+                                "details": "Вторая задача",
+                                "completed": true
+                                }
+                                ]
+                                """)
                 );
     }
+
+    @Test
+    void handleCreateNewTask_PayloadIsInvalid_ReturnsValidResponseEntity() throws Exception {
+        //given
+
+        var requestBuilder = post("/api/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT_LANGUAGE, "en ")
+                .content("""
+                        {
+                        "details": null
+                         }
+                        """);
+        //when
+        this.mockMvc.perform(requestBuilder)
+                //then
+                .andExpectAll(
+                        status().isBadRequest(),
+                        header().doesNotExist(HttpHeaders.LOCATION),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                "errors": ["Task details must be set"]                                }
+                                """, true)
+                );
+
+        assertTrue(this.taskRepository.getTasks().isEmpty());
+
+
+    }
+
+    @Test
+    void handleCreateNewTask_PayloadIsValid_ReturnsValidResponseEntity() throws Exception {
+        //given
+
+        var requestBuilder = post("/api/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                        "details": "Third task"
+                         }
+                        """);
+        //when
+        this.mockMvc.perform(requestBuilder)
+                //then
+                .andExpectAll(
+                        status().isCreated(),
+                        header().exists(HttpHeaders.LOCATION),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                "details": "Third task",
+                                "completed": false
+                                }
+                                """),
+                        jsonPath("$.id").exists());
+        assertEquals(1, this.taskRepository.getTasks().size());
+        final var task = this.taskRepository.getTasks().get(0);
+        assertNotNull(task.id());
+        assertEquals("Third task", task.details());
+        assertFalse(task.completed());
+
+
+    }
 }
+
+
+
